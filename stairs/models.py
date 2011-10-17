45f28024b5bea05c96db7665beed71b18c674c14
zhenyang <nwu.scott119@gmail.com>
2011-10-17 23:54:24 +0800

    models are completed but still no view

--- stairs/models.py
+++ stairs/models.py
@@ -1,10 +1,20 @@
+import datetime
 from django.db import models
 
-class Programmers(models.Model):
+class Programmer(models.Model):
     name = models.TextField()
 
+    def get_count_paired_with(self, programmer_paired_with):
+        return Pairing.objects.filter(programmerOne=self, programmerTwo=programmer_paired_with).count()
+
+    def add_pairing_with(self, programmer_paired_with):
+        if self.id < programmer_paired_with.id:
+            Pairing(programmerOne=self, programmerTwo=programmer_paired_with).save()
+        else:
+            Pairing(programmerOne=programmer_paired_with, programmerTwo=self).save()
+            
 
 class Pairing(models.Model):
-    programmerOne = models.ForeignKey(Programmers, related_name='one')
-    programmerTwo = models.ForeignKey(Programmers, related_name='two')
-    count = models.IntegerField(default=0)
+    programmerOne = models.ForeignKey(Programmer, related_name='one')
+    programmerTwo = models.ForeignKey(Programmer, related_name='two')
+    date = models.DateField(default=datetime.date.today)
--- stairs/tests.py
+++ stairs/tests.py
@@ -6,15 +6,33 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from stairs.models import Programmers
+from stairs.models import Programmer, Pairing
 
 
 class ModelTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        pgr=Programmers(name='asdf')
-        pgr.save()
-        self.assertEqual(pgr.name,'asdf')
-        self.assertIsNotNone(Programmers.objects.get(name='asdf'))
+    def setUp(self):
+        self.yz = Programmer(name='yangzhen')
+        self.yz.save()
+        self.jason = Programmer(name='jason')
+        self.jason.save()
+        self.yz.add_pairing_with(self.jason)
+
+    def test_create_programmer(self):
+        self.assertEqual(self.yz.name, 'yangzhen')
+        self.assertIsNotNone(Programmer.objects.get(name='yangzhen'))
+
+    def test_create_pairing(self):
+        self.assertIsNotNone(Pairing.objects.get(programmerOne=self.yz))
+
+    def test_get_pairing_count(self):
+        self.assertEqual(self.yz.get_count_paired_with(self.jason), 1)
+
+    def test_add_pairing(self):
+        self.yz.add_pairing_with(self.jason)
+        self.assertEqual(self.yz.get_count_paired_with(self.jason), 2)
+        self.jason.add_pairing_with(self.yz)
+        self.assertEqual(self.yz.get_count_paired_with(self.jason), 3)
+
+
+
+        
