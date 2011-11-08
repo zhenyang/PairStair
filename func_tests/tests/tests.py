02163edb629d3a37d4536c669c1943cb12e58ebe
zhenyang <nwu.scott119@gmail.com>
2011-11-08 22:48:35 +0800

    refactor a little function test code

--- func_tests/tests/tests.py
+++ func_tests/tests/tests.py
@@ -11,17 +11,18 @@ class TestCase(unittest.TestCase):
 
     @attr('functional_test')
     def test_should_see_title(self):
-        self.driver.get("http://127.0.0.1:8000/index")
+        self.go_to_index_page()
         self.assertEqual(self.driver.title, "Pair Stair")
 
     @attr('functional_test')
     def test_should_see_table(self):
-        self.driver.get("http://127.0.0.1:8000/index")
+        self.go_to_index_page()
         table = self.driver.find_element_by_css_selector("table")
         self.assertIsNotNone(table)
 
+    @attr('functional_test')
     def test_add_a_programmer(self):
-        self.driver.get("http://127.0.0.1:8000/index")
+        self.go_to_index_page()
         add_programmer_button = self.driver.find_element_by_css_selector("a.add_programmer")
         self.assertIsNotNone(add_programmer_button)
 
@@ -39,8 +40,9 @@ class TestCase(unittest.TestCase):
         table_body = self.driver.find_element_by_css_selector("tbody")
         self.assertIn('Good',table_body.text)
 
+    @attr('functional_test')
     def test_clear_stair(self):
-        self.driver.get("http://127.0.0.1:8000/index")
+        self.go_to_index_page()
         clear_stair_button = self.driver.find_element_by_css_selector("a.clear_stair")
         self.assertIsNotNone(clear_stair_button)
 
@@ -48,6 +50,18 @@ class TestCase(unittest.TestCase):
 
         table_body = self.driver.find_element_by_css_selector("tbody")
         self.assertEqual(table_body.text,'')
+
+    def add_a_programmer(self, programmer_name):
+        self.driver.get("http://127.0.0.1:8000/add_programmer")
+        text_area = self.driver.find_element_by_css_selector("textarea.programmer_name")
+        text_area.send_keys(programmer_name)
+        submit_button = self.driver.find_element_by_css_selector("input.add_programmer")
+        submit_button.click()
+
+    def go_to_index_page(self):
+        self.driver.get("http://127.0.0.1:8000/index")
+
+
 
 if __name__ == '__main__':
     unittest.main()
